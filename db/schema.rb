6cb94f918de49552161ce85df4dd523b94d7a9bd
dfebs <daniel.febles97@gmail.com>
2025-05-21 11:55:31 -0400

    Add username field to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_19_130025) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_21_150012) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -80,7 +80,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_19_130025) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
