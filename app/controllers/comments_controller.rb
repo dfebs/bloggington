3de89eb817c52abfed6252346f84b8e537cdfe1b
dfebs <daniel.febles97@gmail.com>
2025-06-03 12:12:23 -0400

    Add real-time top-level comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,24 @@ class CommentsController < ApplicationController
     @comment = @blog_post.comments.new(comment_params)
     @comment.user = Current.user
 
-    if @comment.save
-      redirect_to @blog_post, notice: "Comment posted!"
-    else
-      redirect_to @blog_post, alert: "Comment failed to post :("
+    unless @comment.save
+      redirect_to @blog_post, status: :unprocessable_entity, alert: "Failed to make comment"
+      return
     end
+
+    respond_to do |format|
+      format.turbo_stream
+
+      # Fallback incase javascript is not enabled (i.e. turbo isn't used and html is expected)
+      format.html do
+        redirect_to @blog_post, notice: "Comment posted"
+      end
+    end
+  end
+
+  def new
+    @comment = Comment.new
+    render partial: "comments/form", locals: { comment: @comment }
   end
 
   private
