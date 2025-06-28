4fb00d666ee12e288d089d4259824bac82d6ddea
dfebs <daniel.febles97@gmail.com>
2025-06-27 20:41:02 -0400

    Add blog post test case and edit user yml

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "does not get created when fields are missing" do
+    blog_post = BlogPost.new
+    assert_not blog_post.save, "Created blog post with missing fields"
+  end
+
+  test "should report error" do
+    assert_raises(NameError) do
+      some_undefined_variable
+    end
+  end
 end
