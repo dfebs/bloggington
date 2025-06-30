491f5efda893020154c9923ceb5f4741fc1c22fd
dfebs <daniel.febles97@gmail.com>
2025-06-29 22:58:19 -0400

    Make action cable work (now requires redis)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,7 @@
 module UsersHelper
+  include Rails.application.routes.url_helpers
+
   def profile_picture(user, max_width)
-    if user.profile_picture.attached?
-      image_tag user.profile_picture, style: "max-width: #{max_width}px;"
-    else
-      image_tag asset_path("defaultpp.png"), style: "max-width: #{max_width}px"
-    end
+    image_tag asset_path("defaultpp.png"), style: "max-width: #{max_width}px;"
   end
 end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -4,4 +4,6 @@ class BlogPost < ApplicationRecord
   has_rich_text :body
 
   validates :title, presence: true
+
+  after_create_commit -> { broadcast_prepend_to "blog_posts", partial: "blog_posts/blog_post", locals: { blog_post: self }, target: "blog_posts" }
 end
